7734f81b45f5a3122036b52a7e15638cc61eb79a
Gabriel N <133405896+Robotgamer324@users.noreply.github.com>
2023-10-18 11:28:11 -0300

    Criação das assinaturas  dos métodos da camada DAL
    o próximo passo vai ser implementar os métodos

---
+++ GestaoComercial/DAL/ProdutoDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ProdutoDAL
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public double Estoque { get; set; }
+    }
+}
---
+++ GestaoComercial/DAL/UsuarioDAL.cs
@@ -0,0 +1,29 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+
+        }
+        public void Alterar(Usuario _usuario)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+        public List<Usuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
